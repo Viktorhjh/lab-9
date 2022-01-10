91113caf3cc4ae5df3f9cbe37d450632621fea83
Viktorhjh <Viktorsek7@gmail.com>
2022-01-10 15:28:26 +0100

    Добавьте файлы проекта.

---
+++ lab 9/ComplexNumber.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_9
+{
+    public class ComplexNumber
+    {
+        double complexNumber;
+        double number;
+
+        public ComplexNumber(double n, double complexN)
+        {
+            number = n;
+            complexNumber = complexN;
+        }
+
+        public double getNumber()
+        {
+            return number;
+        }
+
+        public double getComplexNumber()
+        {
+            return complexNumber;
+        }
+
+        public static ComplexNumber operator + (ComplexNumber n1, ComplexNumber n2)
+        {
+            ComplexNumber sum = new ComplexNumber(n1.number + n2.number, n1.complexNumber + n2.complexNumber);
+            return sum;
+        }
+
+        public static ComplexNumber operator -(ComplexNumber n1, ComplexNumber n2)
+        {
+            ComplexNumber sum = new ComplexNumber(n1.number - n2.number, n1.complexNumber - n2.complexNumber);
+            return sum;
+        }
+
+        public static ComplexNumber operator *(ComplexNumber n1, ComplexNumber n2)
+        {
+            ComplexNumber sum = new ComplexNumber(n1.number*n2.number - n1.complexNumber*n2.complexNumber,
+                n1.complexNumber*n2.number + n2.complexNumber*n1.number);
+            return sum;
+        }
+
+        public static ComplexNumber operator /(ComplexNumber n1, ComplexNumber n2)
+        {
+            ComplexNumber sum = new ComplexNumber(
+                (n1.number * -1 * n2.number - n1.complexNumber * n2.complexNumber)
+                /(-1*(n2.number * n2.number + n2.complexNumber * n2.complexNumber)),
+                (n2.number * n1.complexNumber - n1.number * n2.complexNumber)
+                /((n2.number * n2.number + n2.complexNumber * n2.complexNumber))
+                );            
+            return sum;
+        }
+
+        public static bool operator ==(ComplexNumber n1, ComplexNumber n2)
+        {
+            if (n1.number == n2.number && n1.complexNumber == n2.complexNumber)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator !=(ComplexNumber n1, ComplexNumber n2)
+        {
+            if (n1.number == n2.number && n1.complexNumber == n2.complexNumber)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static bool operator >(ComplexNumber n1, ComplexNumber n2)
+        {
+            if (n1 != n2 && n1.number >= n2.number && n1.complexNumber >= n2.complexNumber)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator <(ComplexNumber n1, ComplexNumber n2)
+        {
+            if (n1.number >= n2.number && n1.complexNumber >= n2.complexNumber)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static implicit operator ComplexNumber(double res)
+        {
+            return new ComplexNumber(res, 0);
+        }
+
+        public static implicit operator double(ComplexNumber res)
+        {
+            return res.number;
+        }
+
+    }
+}
---
+++ lab 9/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_9
+{
+    public partial class Form1 : Form
+    {
+        int index;
+        ComplexNumber n1, n2, res;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            createNumber();
+            calculate();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            index = comboBox1.SelectedIndex;
+        }
+
+        public void createNumber()
+        {
+            n1 = new ComplexNumber(Double.Parse(textBox1.Text), Double.Parse(textBox2.Text));
+            n2 = new ComplexNumber(Double.Parse(textBox3.Text), Double.Parse(textBox4.Text));
+        }
+
+        public void calculate()
+        {
+            switch (index)
+            {
+                case 0:
+                    res = n1 + n2;
+                    if(res.getComplexNumber() > 0)
+                        result.Text = $"{res.getNumber()} + {res.getComplexNumber()}i";
+                    else
+                        result.Text = $"{res.getNumber()} {res.getComplexNumber()}i";
+                    break;
+
+                case 1:
+                    res = n1 - n2;
+                    if (res.getComplexNumber() > 0)
+                        result.Text = $"{res.getNumber()} + {res.getComplexNumber()}i";
+                    else
+                        result.Text = $"{res.getNumber()} {res.getComplexNumber()}i";
+                    break;
+
+                case 2:
+                    res = n1 * n2;
+                    if (res.getComplexNumber() > 0)
+                        result.Text = $"{res.getNumber()} + {res.getComplexNumber()}i";
+                    else
+                        result.Text = $"{res.getNumber()} {res.getComplexNumber()}i";
+                    break;
+
+                case 3:
+                    res = n1 / n2;
+                    if (res.getComplexNumber() > 0)
+                        result.Text = $"{res.getNumber()} + {res.getComplexNumber()}i";
+                    else
+                        result.Text = $"{res.getNumber()} {res.getComplexNumber()}i";
+                    break;
+
+                case 4:
+                    result.Text = $"{n1 == n2}";
+                    break;
+
+                case 5:
+                    result.Text = $"{n1 != n2}";
+                    break;
+
+                case 6:
+                    result.Text = $"{n1 > n2}";
+                    break;
+
+                case 7:
+                    result.Text = $"{n1 < n2}";
+                    break;
+            }
+
+            
+        }
+    }
+}
+/*
+konwersja liczby zespolonej na double i z double na liczbę zespoloną
+indekser (element nr 0 to część rzeczywista, a element nr 1 to część urojona)
+indekser (element nr 'r' to część rzeczywista, a element nr 'i' to część urojona)
+Equals( ) i GetHashCode( )
+ */
